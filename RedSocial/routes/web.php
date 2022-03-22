09629aff8ba272db49c4b27c6acd295d49d45e84
Raúl Vega <raulvf007@gmail.com>
2022-03-22 13:46:30 +0000

    Upload and display images

--- RedSocial/routes/web.php
+++ RedSocial/routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/p/create', 'PostsController@create');
+Route::post('/p', 'PostsController@store');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
