ed108bc6050a2767075bf4ecb4db12724179461c
Joelnavri <jnr_navarro_rivero@hotmail.com>
2022-05-02 16:04:53 +0100

    Created model, controller and migration

--- RedSocial/app/Models/Post.php
+++ RedSocial/app/Models/Post.php
@@ -12,4 +12,8 @@ class Post extends Model
     public function user(){
         $this->belongsTo(User::class);
     }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
